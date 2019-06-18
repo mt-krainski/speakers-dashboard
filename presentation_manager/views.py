f0ac3862b660583b88b5c983dfae1a489f6e7e5f
Mateusz Krainski <mateusz.krainski@showmax.com>
2019-06-18 23:33:27 +0200

    Fix unnecessary import

--- presentation_manager/views.py
+++ presentation_manager/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect, HttpResponseBadRequest
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
