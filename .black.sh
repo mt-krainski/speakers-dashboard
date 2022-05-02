5a7dce516c3565f72fb6cabef2de17d5bd491a58
Mateusz Krainski <mateusz@krainski.eu>
2022-05-02 10:58:18 +0200

    Make runnable again, move to poetry

--- .black.sh
+++ .black.sh
@@ -3,7 +3,7 @@ for file in `git status -s | sed "s/^[[:space:]]*.*?[[:space:]]*//"`
 do
         if [[ $file =~ \.py$ ]] ; then
                 echo "Running for ${file}"
-                black $file --line-length 79 --target-version py37
+                black $file --target-version py39
                 git add $file
         fi
 done
